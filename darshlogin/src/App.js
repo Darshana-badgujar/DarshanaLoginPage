eb9923658004cccfa18c487e7aff79f176c3723a
Darshana-badgujar <darshanabadgujar272@gmail.com>
2024-11-13 17:34:26 +0530

    pages added

--- darshlogin/src/App.js
+++ darshlogin/src/App.js
@@ -1,14 +1,26 @@
-//import {BrowserRouter,Routes,Route } from 'react-router-dom' ;
+import {BrowserRouter,Routes,Route } from 'react-router-dom' ;
 
 import logo from './logo.svg';
 import './App.css';
-import Login from'./Component/Pages/Login';
+import Login from './Component/Pages/Login'
 import Register from './Component/Pages/Register';
 function App() {
   return (
    //< Register/>
-    <Login/>
-  );
+  //  <Login/>
+    <BrowserRouter>
+    <Routes>
+     
+     
+
+     
+     <Route path='/'  element={<Login/>}/>
+     
+     <Route path='/Register'element={<Register/>}/>
+    </Routes>
+     </BrowserRouter>
+  ) ;
+  
 }
 
 export default App;
